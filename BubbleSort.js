5058ba58d2734f23d13f790f7c7134ec925f6396
dina-shoham <dina.shoham@gmail.com>
2024-10-18 10:51:20 -0400

    optimized a bit

--- BubbleSort.js
+++ BubbleSort.js
@@ -15,16 +15,21 @@ rl.question(">>Enter array, using spaces to separate values (e.g. 1 2 3): ", fun
 function BubbleSort(inputString) {
     arr = inputString.split(" ");
 
-    let i, j, swap;
+    let i, j, swap, swapped;
     let n = arr.length;
 
     for (i = 0; i < n-1; i++) {
+        swapped = false;
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap = arr[j + 1];
                 arr[j + 1] = arr [j];
                 arr[j] = swap;
+                swapped = true;
             }
+        }
+        if (!swapped) {
+            break;
         }
     }
 
